c5531498fca3733c12fc00ba6903a80633671b96
Jeff Wear <jeff@mixmax.com>
2016-08-06 02:16:50 -0700

    Streaming API

--- spec/node/indexSpec.js
+++ spec/node/indexSpec.js
@@ -1,6 +1,14 @@
+var es = require('event-stream');
+var fs = require('fs');
+var JSONStream = require('JSONStream');
+var mockFs = require('mock-fs');
 var UserGate = require('../../src/node');
 
 describe('UserGate', function() {
+  afterEach(function() {
+    mockFs.restore();
+  });
+
   it('should encode the specified user', function() {
     expect(new UserGate({
       userList: ['jeff@mixmax.com']
@@ -18,5 +26,102 @@ describe('UserGate', function() {
         'QQVkG1DSbHvP7amX9oSdmi3l+CI/ivz9lrEFyMX91gI='
       ]
     });
+  });
+
+  it('should encode zero users', function() {
+    expect(new UserGate({ userList: [] }).toJSON()).toEqual({ userList: [] });
+    expect(new UserGate({}).toJSON()).toEqual({ userList: [] });
+    expect(new UserGate().toJSON()).toEqual({ userList: [] });
+  });
+
+  it('should write JSON stringification to stream', function(done) {
+    var gate = new UserGate({
+      userList: ['jeff@mixmax.com', 'bar@mixmax.com']
+    });
+
+    // Explicitly end the stream since we're not going to write any additional users to it.
+    gate.toStream({ end: true })
+      .pipe(es.wait(function(err, jsonString) {
+        if (err) {
+          done.fail(err);
+        } else {
+          expect(JSON.parse(jsonString)).toEqual({
+            userList: [
+              'vm94x1WIA09ozQJA9VwuD1enx8ZncM8v5ztE04zGSDU=',
+              'QQVkG1DSbHvP7amX9oSdmi3l+CI/ivz9lrEFyMX91gI='
+            ]
+          });
+          done();
+        }
+      }));
+  });
+
+  it('should handle writing an empty array to the stream', function(done) {
+    var gate = new UserGate({
+      userList: []
+    });
+
+    // Explicitly end the stream since we're not going to write any additional users to it.
+    gate.toStream({ end: true })
+      .pipe(es.wait(function(err, jsonString) {
+        if (err) {
+          done.fail(err);
+        } else {
+          expect(JSON.parse(jsonString)).toEqual({ userList: [] });
+          done();
+        }
+      }));
+  });
+
+  it('should read users from stream', function(done) {
+    mockFs({
+      'users.json': JSON.stringify(['jeff@mixmax.com', 'bar@mixmax.com'])
+    });
+
+    var gate = new UserGate();
+
+    fs.createReadStream('users.json', 'utf8')
+      .pipe(JSONStream.parse('*'))
+      .pipe(gate.toStream())
+      .pipe(es.wait(function(err, jsonString) {
+        if (err) {
+          done.fail(err);
+        } else {
+          expect(JSON.parse(jsonString)).toEqual({
+            userList: [
+              'vm94x1WIA09ozQJA9VwuD1enx8ZncM8v5ztE04zGSDU=',
+              'QQVkG1DSbHvP7amX9oSdmi3l+CI/ivz9lrEFyMX91gI='
+            ]
+          });
+          done();
+        }
+      }));
+  });
+
+  it('should include initial users when reading users from stream', function(done) {
+    mockFs({
+      'users.json': JSON.stringify(['bar@mixmax.com'])
+    });
+
+    var gate = new UserGate({
+      userList: ['jeff@mixmax.com']
+    });
+
+    fs.createReadStream('users.json', 'utf8')
+      .pipe(JSONStream.parse('*'))
+      .pipe(gate.toStream())
+      .pipe(es.wait(function(err, jsonString) {
+        if (err) {
+          done.fail(err);
+        } else {
+          expect(JSON.parse(jsonString)).toEqual({
+            userList: [
+              'vm94x1WIA09ozQJA9VwuD1enx8ZncM8v5ztE04zGSDU=',
+              'QQVkG1DSbHvP7amX9oSdmi3l+CI/ivz9lrEFyMX91gI='
+            ]
+          });
+          done();
+        }
+      }));
   });
 });
--- src/node/index.js
+++ src/node/index.js
@@ -1,21 +1,48 @@
 var crypto = require('crypto');
+var es = require('event-stream');
+var JSONStream = require('JSONStream');
 
 function UserGate(options) {
+  options = options || {};
   this._userList = options.userList || [];
 }
 
 Object.assign(UserGate.prototype, {
   toJSON: function() {
     return {
-      userList: this._encodedUserList()
+      userList: this._userList.map(encodeUser)
     };
   },
 
-  _encodedUserList: function() {
-    return this._userList.map((user) => {
-      return crypto.createHash('sha256').update(user).digest('base64');
-    });
+  toStream: function(options) {
+    options = options || {};
+
+    // Only end the stream (see below) if the client's not going to write any additional users to it.
+    var endStream = options.end || false;
+
+    // Mimic the structure returned by `toJSON`. `JSONStream.stringify` will inject
+    // new elements in the middle.
+    var op = '{"userList":[',
+        sep = ',',
+        cl = ']}';
+
+    // `es.pipeline` pipes these streams together but causes writes to go to the first
+    // stream and reads to come from the last, whereas if we just returned the result of
+    // `pipe` both reads and writes would target the last stream.
+    var pipeline = es.pipeline(
+      es.mapSync(encodeUser),
+      JSONStream.stringify(op, sep, cl)
+    );
+
+    // Inject the initial users.
+    es.readArray(this._userList).pipe(pipeline, { end: endStream });
+
+    return pipeline;
   }
 });
+
+function encodeUser(user) {
+  return crypto.createHash('sha256').update(user).digest('base64');
+}
 
 module.exports = UserGate;
