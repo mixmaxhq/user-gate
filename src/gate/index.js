7e6dfa937363205c20409112b4cd4f9e5553baf0
Srinand Balaji <srinand@mixmax.com>
2019-05-06 16:20:24 -0700

    consolidates `_matchesUniformSample` logic into `_matchesSample` logic
    In other words, removes all logic related to `sampleCharacterSet` comparisons
    and replaces it with hashing and comparing hashes.

--- src/gate/index.js
+++ src/gate/index.js
@@ -1,11 +1,17 @@
 const BloomFilter = require('bloom-filter-remixed');
+/**
+ * `sha.js` is an isomorphic hashing library that implements its hashing functions synchronously.
+ *  This is important for our usage of it, as we wish to support this library without resorting to
+ *  browser-only or node-only APIs.
+ *
+ *  We could use `createHash`, which is a top level dependency of browserify's crypto polyfill -
+ *  however, adding said library to this module bloats the bundle size to around 150 kb. We can
+ *  avoid this by only using the hashing function we need and make use of browserify's Buffer
+ *  polyfill for anything after that.
+ */
 const sha256 = require('sha.js/sha256')
 
 const MAX_UINT_32 = Math.pow(2, 32);
-
-function _isFalseyVal(val) {
-  return val === undefined || val === null;
-}
 
 /**
  * Deserializes a user gate and checks users against the gate.
@@ -14,22 +20,19 @@ function _isFalseyVal(val) {
  *
  * @param {Object} encodedGate - JSON produced by `UserGateEncoder`.
  * @param {Object=} options
- *   @property {String=} _sampleCharacterSet_: The string of characters with which the unencoded
- *     user identifiers could begin. Defaults to `'abcdefghijklmnopqrstuvwxyz'` which works for
- *     identifiers that are emails. Case-insensitive. See `UserGate#allows` for how this is used.
+ *   @property {float=} sample - the percentage of users we wish to let in.
  */
 class UserGate {
   constructor(encodedGate, options) {
-  encodedGate = encodedGate || {};
+    encodedGate = encodedGate || {};
 
-  if (encodedGate.list) {
-    encodedGate.list.vData = new Buffer(encodedGate.list.vData.data);
-    this._list = new BloomFilter(encodedGate.list);
-  }
-  this._sample = encodedGate.sample;
+    if (encodedGate.list) {
+      encodedGate.list.vData = new Buffer(encodedGate.list.vData.data);
+      this._list = new BloomFilter(encodedGate.list);
+    }
+    this._sample = encodedGate.sample;
 
-  options = options || {};
-  this._sampleCharacterSet = options.sampleCharacterSet || 'abcdefghijklmnopqrstuvwxyz';
+    options = options || {};
   }
 
   /**
@@ -39,15 +42,9 @@ class UserGate {
    * - they're on the list
    * - they're part of the first _sample_ users
    *
-   * Sampling is done by checking the character with which _user_ begins. For example,
-   * if _sample_ is `0.5` and the gate uses the default _sampleCharacterSet_, _user_ would be
-   * allowed through the gate if it began with any character between `a-n` (halfway through
-   * the alphabet).
-   *
-   * Users are unlikely to be uniformly distributed over _sampleCharacterSet_ and
-   * thus a _sample_ of `0.5` won't exactly select for 50% of users. However, this
-   * sampling technique is deterministic: a user will either always be allowed through
-   * the gate, even if they reload, or they never will.
+   * Sampling is done by hashing the user string and projecting it onto a sample space
+   * (more on this below). This sampling technique is deterministic: a user will either
+   * always be allowed through the gate, even if they reload, or they never will.
    *
    * Checking against the list requires an exact match. However, sampling is
    * case-insensitive.
@@ -57,12 +54,7 @@ class UserGate {
    * @return {Boolean} `true` if _user_ is allowed through the gate, `false` otherwise.
    */
   allows(user) {
-    // Micro-optimization: check `_matchesSample` first because it's faster.
     return this._matchesSample(user) || this._matchesList(user);
-  }
-
-  allowsUniform(user) {
-    return (this._matchesUniformSample(user) || this._matchesList(user));
   }
 
   _matchesList(user) {
@@ -72,18 +64,12 @@ class UserGate {
   }
 
   _matchesSample(user) {
-    if (_isFalseyVal(this._sample) || !((this._sample >= 0) && (this._sample <= 1))) return false;
+    if ((this._sample === undefined || this._sample === null) || !((this._sample >= 0) && (this._sample <= 1))) return false;
 
-    // See if the user begins with a character in the sample set.
-    const effectiveCharacterSet = this._sampleCharacterSet.slice(
-      0, Math.round(this._sampleCharacterSet.length * this._sample));
+    if (!user) return false;
 
-    const userMatches = new RegExp('^[' + effectiveCharacterSet + ']', 'i').test(user);
-    return userMatches;
-  }
-
-  _matchesUniformSample(user) {
-    if (_isFalseyVal(this._sample) || !((this._sample >= 0) && (this._sample <= 1))) return false;
+    // Allow for case insensitivity, which was a feature in V2.0 of this library.
+    user = user.toLowerCase();
 
     // We've got to project `user` onto the sample space (i.e. convert it to a number between 0 and
     // 1) in a way that is a) deterministic b) uniform. We do this by hashing the user, converting it
